988b222ae036375e931497dc869bd11f3983dc41
tulindanil <tulindanil@gmail.com>
2016-03-04 12:03:17 +0300

    Initial commit

---
+++ Seminars/03-04-2016/main.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+
+std::stack<int> st;
+std::mutex mut;
+
+template <typename T>
+class CMyStack {
+
+public:
+
+    CMyStack() {
+    }
+
+    void push(const T &obj) {
+        std::unique_lock<std::mutex> lock(mutex, std::defer_lock_t());
+        intern.push(obj);
+        lock.unlock();
+        cv.notify_one();
+    }
+
+    std::unique_ptr<T> wait_pop() {
+        std::unique_lock<std::mutex> lock(mutex, std::defer_lock_t());
+
+        if (intern.size() > 0) {
+            return exclusive_pop();
+        } else {
+            cv.wait(lock, [&]() {
+                return intern.size() > 0;
+            });
+            return exclusive_pop();
+        }
+    }
+
+
+    bool try_pop(T &out) {
+        std::unique_lock<std::mutex> lock(mutex);
+
+        if (intern.size() > 0) {
+            out = intern.top();
+            intern.pop();
+            return true;
+        }
+
+        return false;
+    }
+
+
+    size_t size() const;
+
+private:
+
+    std::stack<T> intern;
+    std::mutex mutex;
+
+    std::condition_variable cv; 
+
+    std::unique_ptr<T> exclusive_pop() {
+        std::unique_ptr<T> result(new T(intern.top()));
+        intern.pop();
+        return result;
+    }
+     
+
+};
+
+void f(int);
+
+int main() {
+
+    CMyStack<int> stack;
+
+    return 0;
+}
