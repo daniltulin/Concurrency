c7a1c763a69c13d139caeec0fdb41010973cd1a6
tulindanil <tulindanil@gmail.com>
2016-03-15 12:46:25 +0300

    baseline test for thread safe queue

--- HomeWorks/ThreadPool/BlockingQueue/test.cc
+++ HomeWorks/ThreadPool/BlockingQueue/test.cc
@@ -4,6 +4,11 @@
 #define BOOST_TEST_MODULE Blocking Queue tests
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(free_test_function) {
-      BOOST_TEST(true);
+BOOST_AUTO_TEST_CASE(baseline_test) {
+    int value = 7;
+    thread_safe_queue<int> queue(10);
+    BOOST_TEST(queue.enqueue(value));
+    int popped;
+    BOOST_TEST(queue.pop(popped));
+    BOOST_TEST(popped == value); 
 }
