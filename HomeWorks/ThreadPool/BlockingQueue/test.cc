68b4c97eb4287124736b64c6b99851b2bb048ad1
tulindanil <tulindanil@gmail.com>
2016-03-15 15:05:08 +0300

    add fixture class for all baseline tests

--- HomeWorks/ThreadPool/BlockingQueue/test.cc
+++ HomeWorks/ThreadPool/BlockingQueue/test.cc
@@ -5,9 +5,26 @@
 #define BOOST_TEST_MODULE Blocking Queue tests
 #include <boost/test/included/unit_test.hpp>
 
+class baseline_fixture {
+
+public:
+
+    thread_safe_queue<int> queue;
+
+    baseline_fixture(): queue(1000) {
+
+    }
+
+    ~baseline_fixture() {
+
+    }
+
+};
+
+BOOST_FIXTURE_TEST_SUITE(baseline_tests, baseline_fixture)
+
 BOOST_AUTO_TEST_CASE(baseline_test) {
     int value = 7;
-    thread_safe_queue<int> queue(10);
     BOOST_TEST(queue.enqueue(value));
     int popped;
     BOOST_TEST(queue.pop(popped));
@@ -15,20 +32,21 @@ BOOST_AUTO_TEST_CASE(baseline_test) {
 
     queue.enqueue(value);
     queue.shutdown();
+    BOOST_TEST(queue.enqueue(value) == false);
     BOOST_TEST(queue.pop(popped) == false);
 }
 
 BOOST_AUTO_TEST_CASE(one_thread_deque_test) {
-    size_t size = 1000;
-    thread_safe_queue<int> queue(size);
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < queue.get_capacity(); ++i) {
         BOOST_TEST(queue.enqueue(i));
     }
 
     int popped;
-   for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < queue.get_capacity(); ++i) {
         popped = 0;
         BOOST_TEST(queue.pop(popped));
         BOOST_TEST(popped == i);
     }
 }
+
+BOOST_AUTO_TEST_SUITE_END()
