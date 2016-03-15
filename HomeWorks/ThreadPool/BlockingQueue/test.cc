5dfffda335054bb63fb62965f973f6e3557f43f2
tulindanil <tulindanil@gmail.com>
2016-03-15 13:13:52 +0300

    one_thread_deque_test is created

--- HomeWorks/ThreadPool/BlockingQueue/test.cc
+++ HomeWorks/ThreadPool/BlockingQueue/test.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 #include "thread_safe_queue.hpp"
 
 #define BOOST_TEST_MODULE Blocking Queue tests
@@ -11,4 +12,23 @@ BOOST_AUTO_TEST_CASE(baseline_test) {
     int popped;
     BOOST_TEST(queue.pop(popped));
     BOOST_TEST(popped == value); 
+
+    queue.enqueue(value);
+    queue.shutdown();
+    BOOST_TEST(queue.pop(popped) == false);
+}
+
+BOOST_AUTO_TEST_CASE(one_thread_deque_test) {
+    size_t size = 1000;
+    thread_safe_queue<int> queue(size);
+    for (size_t i = 0; i < size; ++i) {
+        BOOST_TEST(queue.enqueue(i));
+    }
+
+    int popped;
+   for (size_t i = 0; i < size; ++i) {
+        popped = 0;
+        BOOST_TEST(queue.pop(popped));
+        BOOST_TEST(popped == i);
+    }
 }
