3862d293f4379abd590272c0f07c9a02db120f4e
tulindanil <tulindanil@gmail.com>
2016-03-19 00:06:58 +0300

    script is done

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,8 +1,8 @@
+!/bin/bash
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
-echo $CPLUS_INCLUDE_PATH
 mv download boost.tar.gz
 gzip -d boost.tar.gz
 tar xvf boost.tar
 cd boost_1_60_0/
 export CPLUS_INCLUDE_PATH="$(pwd):$CPLUS_INCLUDE_PATH"
-echo CPLUS_INCLUDE_PATH
+echo $CPLUS_INCLUDE_PATH
