56afe1157a6deb557dc46395785ad654f8a05de1
tulindanil <tulindanil@gmail.com>
2016-03-19 00:15:33 +0300

    verbose tar

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -2,7 +2,7 @@
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
 gzip -d boost.tar.gz
-tar xvf boost.tar
+tar xvf boost.tar -v
 cd boost_1_60_0/
 export CPLUS_INCLUDE_PATH="$(pwd):$CPLUS_INCLUDE_PATH"
 echo $CPLUS_INCLUDE_PATH
