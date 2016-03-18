16b465a6c453a445d2172633c8ad4c0133f969f0
tulindanil <tulindanil@gmail.com>
2016-03-19 00:39:15 +0300

    This is a basic implementation that works.

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -2,7 +2,9 @@
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
 gzip -d boost.tar.gz
-tar xvf boost.tar 2>0
+tar xvf boost.tar > /dev/null
 cd boost_1_60_0/
 export CPLUS_INCLUDE_PATH="$(pwd):$CPLUS_INCLUDE_PATH"
+export CPATH=$CPLUS_INCLUDE_PATH
 echo $CPLUS_INCLUDE_PATH
+echo $CPATH
