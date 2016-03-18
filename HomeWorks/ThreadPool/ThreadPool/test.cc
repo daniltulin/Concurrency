d5bec4645b3d1e78dc5401107207e7688f9c3e0a
tulindanil <tulindanil@gmail.com>
2016-03-18 10:47:01 +0300

    Initial commit

---
+++ HomeWorks/ThreadPool/ThreadPool/test.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <future>
+
+#include "thread_pool.hpp"
+
+#define BOOST_TEST_MODULE thread pool tests
+#include <boost/test/included/unit_test.hpp>
+
+namespace utf = boost::unit_test;
+
