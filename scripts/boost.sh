596976aeb6ca2cdd6f4e84af3b3ba899bd47e761
tulindanil <tulindanil@gmail.com>
2016-03-19 00:18:44 +0300

    echo at the start

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,4 +1,5 @@
-!/bin/bash
+#!/bin/bash
+echo $CPLUS_INCLUDE_PATH
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
 gzip -d boost.tar.gz
