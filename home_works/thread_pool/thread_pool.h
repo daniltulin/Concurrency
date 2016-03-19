00bdf03a28c1b4123a5d7176e19d6c408dae8401
tulindanil <tulindanil@gmail.com>
2016-03-20 00:57:59 +0300

    structure is ready

--- home_works/thread_pool/thread_pool.h
+++ home_works/thread_pool/thread_pool.h
@@ -16,7 +16,20 @@ public:
 
 private:
 
-    size_t size;
     static size_t get_def_size();
+
+    std::vector<std::thread> workers;
+
+    std::atomic_bool should_shutdown;
+
+    thread_safe_queue<package> queue;
+
+    class package {
+        package();
+        std::promise<T> promise;
+        std::function<T> task;
+    };
+
+    void launch_workers();
 
 };
--- home_works/thread_pool/thread_pool.hpp
+++ home_works/thread_pool/thread_pool.hpp
@@ -1,28 +1,39 @@
 #include "thread_pool.h"
 
 template<typename T>
-thread_pool<T>::thread_pool(): size(get_def_size()){
-
+thread_pool<T>::thread_pool(): workers(get_def_size()){
+    launch_workers();
 }
 
 template<typename T>
-thread_pool<T>::thread_pool(size_t size): size(size) {
+thread_pool<T>::thread_pool(size_t size): workers(size) {
+    launch_workers();
+}
 
+template<typename T>
+void thread_pool<T>::launch_workers() {
 }
 
 template<typename T>
 std::future<T> thread_pool<T>::submit(const std::function<T>& task) {
+   package worker_package(task); 
+   queue.push(worker_package);
+   return worker_task.promise.get_future();
+}
+
+template<typename T>
+thread_pool<T>::package(const std::function<T>& task): task(task) {
 
 }
 
 template<typename T>
 size_t thread_pool<T>::getSize() const {
-    return size;
+    return workers.size();
 }
 
 template<typename T>
 void thread_pool<T>::shutdown() {
-
+    should_shutdown = true;
 }
 
 template<typename T>
