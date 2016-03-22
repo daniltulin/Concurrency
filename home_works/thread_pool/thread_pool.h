0be0aa920fed64d865c5e852faef6738e6a79383
tulindanil <tulindanil@gmail.com>
2016-03-23 01:46:30 +0300

    add namespace, thread_run method, queue

--- home_works/thread_pool/thread_pool.h
+++ home_works/thread_pool/thread_pool.h
@@ -1,15 +1,29 @@
 #include <future>
 #include "thread_safe_queue.hpp"
 
+namespace concurrency {
+
+template<typename T>
+struct packaged_task {
+    packaged_task() = default;
+    packaged_task(const std::function<T()>& task);
+    packaged_task(packaged_task&& rhs);
+    packaged_task& operator=(packaged_task&& rhs);
+    std::promise<T> promise;
+    std::function<T()> task;
+};
+
+
 template<typename T>
 class thread_pool {
 
 public:
 
     thread_pool();
+    ~thread_pool();
     thread_pool(size_t size);
 
-    std::future<T> submit(const std::function<T>& task);
+    std::future<T> submit(const std::function<T()>& task);
 
     inline size_t getSize() const;
     inline void shutdown();
@@ -20,15 +34,15 @@ private:
 
     std::vector<std::thread> workers;
     std::atomic_bool should_shutdown;
-    thread_safe_queue<package> queue;
-
-    class package {
-        package();
-        std::promise<T> promise;
-        std::function<T> task;
-    };
+    thread_safe_queue<packaged_task<T>> queue;
 
     void launch_workers();
+
+    void thread_run();
     void worker_loop();
 
 };
+
+}
+
+using concurrency::thread_pool;
