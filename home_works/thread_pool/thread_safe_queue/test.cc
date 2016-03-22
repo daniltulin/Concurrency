684d93d2e85132ccf43ef626f80d3595a471a936
tulindanil <tulindanil@gmail.com>
2016-03-22 22:26:54 +0300

    int -> size_t

--- home_works/thread_pool/thread_safe_queue/test.cc
+++ home_works/thread_pool/thread_safe_queue/test.cc
@@ -12,7 +12,7 @@ class baseline_fixture {
 
 public:
 
-    thread_safe_queue<int> queue;
+    thread_safe_queue<size_t> queue;
 
     baseline_fixture(): queue(1000) {
 
@@ -27,9 +27,9 @@ public:
 BOOST_FIXTURE_TEST_SUITE(baseline_tests, baseline_fixture)
 
 BOOST_AUTO_TEST_CASE(baseline_test) {
-    int value = 7;
+    size_t value = 7;
     BOOST_TEST(queue.enqueue(value));
-    int popped;
+    size_t popped;
     BOOST_TEST(queue.pop(popped));
     BOOST_TEST(popped == value); 
 
@@ -46,7 +46,7 @@ BOOST_AUTO_TEST_CASE(one_thread_deque_test) {
         BOOST_TEST(queue.enqueue(i));
     }
 
-    int popped;
+    size_t popped;
     for (size_t i = 0; i < queue.get_capacity(); ++i) {
         popped = 0;
         BOOST_TEST(queue.pop(popped));
@@ -60,7 +60,7 @@ class summing_fixture {
 public:
 
     size_t workers_qty;
-    thread_safe_queue<int> queue;
+    thread_safe_queue<size_t> queue;
     size_t sum;
 
     summing_fixture(): workers_qty(4), 
@@ -69,9 +69,10 @@ public:
 
     }
 
-    static size_t summing(thread_safe_queue<int>& queue) {
-        size_t sum = 0;
-        int popped = 0;
+    template<typename T>
+    static T summing(thread_safe_queue<T>& queue) {
+        T sum = 0;
+        T popped = 0;
         bool working = true;
         while(queue.pop(popped)) {
             sum += popped;
@@ -96,7 +97,7 @@ BOOST_AUTO_TEST_CASE(summing_test, *utf::timeout(5)) {
     push(queue.get_capacity());
 
     BOOST_TEST_CHECKPOINT("is going to launch threads");
-    auto task = std::bind(summing_fixture::summing, std::ref(queue));
+    auto task = std::bind(summing_fixture::summing<size_t>, std::ref(queue));
     for (auto it = futures.begin(); it != futures.end(); ++it) {
         auto future = std::async(std::launch::async, task);
         *it = std::move(future);
