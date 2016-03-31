ee94cc24e795749aa87bc362c5292fbf172adbe3
tulindanil <tulindanil@gmail.com>
2016-04-01 00:52:43 +0300

    update URL

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
+wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz?r=&ts=1459461116&use_mirror=nchc 
+#https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
 gzip -d boost.tar.gz
 tar xvf boost.tar > /dev/null
