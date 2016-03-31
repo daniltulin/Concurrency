332477bf5e13931595908363fd5756846545aa5b
tulindanil <tulindanil@gmail.com>
2016-04-01 00:55:29 +0300

    bring it back

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz?r=&ts=1459461116&use_mirror=nchc 
-#https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
+wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
 gzip -d boost.tar.gz
 tar xvf boost.tar > /dev/null
