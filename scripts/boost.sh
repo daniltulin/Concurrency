c96c1a825272ece83fa873650be25f0c0cdbc879
tulindanil <tulindanil@gmail.com>
2016-04-01 01:00:05 +0300

    modify script

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
-gzip -d boost.tar.gz
-tar xvf boost.tar > /dev/null
-cd boost_1_60_0/
-echo $CPLUS_INCLUDE_PATH
+gzip -d boost.tar.gz && tar xvf boost.tar > /dev/null
