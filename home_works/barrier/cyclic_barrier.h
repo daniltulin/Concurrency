81f79b1088fb85dedc938f2830d0937736636a91
tulindanil <tulindanil@gmail.com>
2016-04-22 12:09:58 +0300

    implement cyclic_barrier

--- home_works/barrier/cyclic_barrier.h
+++ home_works/barrier/cyclic_barrier.h
@@ -5,9 +5,19 @@ class cyclic_barrier {
 
 public:
 
+    cyclic_barrier(size_t size);
     void enter();
 
 private:
 
+    typedef std::mutex mutex_type;
+
+    size_t size;
+    size_t threads_num;
+
+    size_t era;
+
+    std::mutex mutex;
+    std::condition_variable cv;
 
 };
--- home_works/barrier/cyclic_barrier.hpp
+++ home_works/barrier/cyclic_barrier.hpp
@@ -1,5 +1,24 @@
 #include "cyclic_barrier.h"
 
+cyclic_barrier::cyclic_barrier(size_t size): size(size) {
+
+}
+
 void cyclic_barrier::enter() {
+
+    std::unique_lock<mutex_type> lock(mutex);
+    size_t thread_era = era;
+    threads_num++;
+
+    if (threads_num == size) {
+        era++;
+        cv.notify_all();
+        threads_num = 0;
+        return;
+    }
+
+    cv.wait(lock, [&]() {
+        return era != thread_era;
+    });
 
 }
