fa5219064ae82a7eca179dd1069f34629d63b12f
tulindanil <tulindanil@gmail.com>
2016-04-26 02:10:17 +0300

    remove verbous output

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
 mv download boost.tar.gz
-gzip -d boost.tar.gz && tar xvf boost.tar > /dev/null
+gzip -d boost.tar.gz && tar xf boost.tar 
